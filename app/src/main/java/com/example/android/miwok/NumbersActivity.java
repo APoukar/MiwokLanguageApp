fb713af940f65dc2f43f78e44a79befd4133e76c
AdamP <adam.poukar@ceskesperky.cz>
2018-02-10 21:50:08 +0100

    Show english numbers in the NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,6 +18,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -39,6 +41,13 @@ public class NumbersActivity extends AppCompatActivity {
         englishNumbers.add("Eight");
         englishNumbers.add("Nine");
         englishNumbers.add("Ten");
+
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        for (int i = 0; i < englishNumbers.size(); i++) {
+            String textViewName = "textView" + i;
+            TextView numberTextView = new TextView(this);
+            numberTextView.setText(englishNumbers.get(i));
+            rootView.addView(numberTextView);
         }
     }
 }
